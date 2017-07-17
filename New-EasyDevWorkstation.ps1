736a22bc5feab9e697be03a9a2de0aa4782fb91d
Nissan Dookeran <nissan.dookeran@thesolutioncollective.com.au>
2017-07-17 15:07:04 +1000

    Add Microsoft Azure Storage Explorer to o365/azure setup

--- New-EasyDevWorkstation.ps1
+++ New-EasyDevWorkstation.ps1
@@ -13,4 +13,5 @@ refreshenv
 
 #Install the basics - Git, Chrome browser, updated Powershell (if running on older Windows like Win Server 2008R2)
 # Visual Studio Code (which is free), and latest SQL Server Management Studio (also free)
-choco install -y --allow-empty-checksums git GoogleChrome powershell visualstudiocode sql-server-management-studio postman fiddler4
+# Microsoft Azure Storage Explorer
+choco install -y --allow-empty-checksums git GoogleChrome powershell visualstudiocode sql-server-management-studio postman fiddler4 microsoftazurestorageexplorer
